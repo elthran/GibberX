47749aed48ee3a248db045215dc773db3ca74692
elthran <jacobbrunner@gmail.com>
2018-09-08 17:18:01 -0700

    You can now choose between two books. Correct book title is passed as paramter to the reading screen.

--- app/src/main/java/elthran/gibberx/OpenBookActivity.java
+++ app/src/main/java/elthran/gibberx/OpenBookActivity.java
@@ -1,8 +1,10 @@
 package elthran.gibberx;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class OpenBookActivity extends AppCompatActivity {
 
@@ -15,5 +17,10 @@ public class OpenBookActivity extends AppCompatActivity {
         Bundle b = getIntent().getExtras();
         bookName = b.getString("bookName");
         Log.v("SecretMessage", bookName);
+    }
+
+    public void returnToBookSelection(View view) {
+        Intent intent = new Intent(this, ChooseBookActivity.class);
+        startActivity(intent);
     }
 }
