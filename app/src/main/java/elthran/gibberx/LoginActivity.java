38841f46cb3b6516be6d34157b61a3d2769faf9b
elthran <jacobbrunner@gmail.com>
2018-09-09 14:11:58 -0700

    User has a class. A JSON dict gets sent to server. But not working fully.

--- app/src/main/java/elthran/gibberx/LoginActivity.java
+++ app/src/main/java/elthran/gibberx/LoginActivity.java
@@ -3,11 +3,31 @@ package elthran.gibberx;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.EditText;
 import android.widget.Button;
 import android.widget.Toast;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.SerializedName;
+import okhttp3.MultipartBody;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import org.json.JSONObject;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.*;
+
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,7 +35,60 @@ public class LoginActivity extends AppCompatActivity {
     private static EditText password;
     private static TextView attempt;
     private static Button login_button;
-    int attempt_counter=5;
+    int attempt_counter = 5;
+
+    public class User {
+
+        private int id;
+        private String name;
+        private String password;
+
+        public User(int id, String name, String password){
+            this.id = id;
+            this.name = name;
+            this.password = password;
+        }
+    }
+    public static final String BASE_URL = "http://gibberx.pythonanywhere.com/login";
+
+    public void sendPost() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(BASE_URL);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                    conn.setRequestProperty("Accept","application/json");
+                    conn.setDoOutput(true);
+                    conn.setDoInput(true);
+
+                    JSONObject jsonParam = new JSONObject();
+                    jsonParam.put("username", "example_user");
+                    jsonParam.put("password", "example_password");
+
+                    Log.i("JSON", jsonParam.toString());
+                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+                    //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
+                    os.writeBytes(jsonParam.toString());
+
+                    os.flush();
+                    os.close();
+
+                    Log.i("STATUS", String.valueOf(conn.getResponseCode()));
+                    Log.i("MSG" , conn.getResponseMessage());
+                    Log.v("MSG" , "FUCK YOU");
+
+                    conn.disconnect();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,35 +97,35 @@ public class LoginActivity extends AppCompatActivity {
         LoginButton();
     }
 
-    public void LoginButton(){
-        username = (EditText)findViewById(R.id.editText_username);
-        password = (EditText)findViewById(R.id.editText_password);
-        attempt = (TextView)findViewById(R.id.textView_attempt_count);
-        login_button = (Button)findViewById(R.id.button_login);
+    public void LoginButton() {
+        username = (EditText) findViewById(R.id.editText_username);
+        password = (EditText) findViewById(R.id.editText_password);
+        attempt = (TextView) findViewById(R.id.textView_attempt_count);
+        login_button = (Button) findViewById(R.id.button_login);
 
         attempt.setText(Integer.toString(attempt_counter));
 
-        login_button.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if (username.getText().toString().equals("") &&
-                        password.getText().toString().equals("")){
-                            Toast.makeText(LoginActivity.this,"Username and password is correct",
-                                    Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                            startActivity(intent);
-                        }
-                        else {
-                            Toast.makeText(LoginActivity.this,"Username and password is NOT correct",
-                                    Toast.LENGTH_SHORT).show();
-                            attempt_counter--;
-                            attempt.setText(Integer.toString(attempt_counter));
-                            if(attempt_counter==0)
-                                login_button.setEnabled(false);
-                        }
+            login_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    User user = new User(1, username.getText().toString(), password.getText().toString());
+                    sendPost();
+
+                    if (user.name.equals("") && user.password.equals("")) {
+                        Toast.makeText(LoginActivity.this, "Username and password is correct",
+                                Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Username and password is NOT correct",
+                                Toast.LENGTH_SHORT).show();
+                        attempt_counter--;
+                        attempt.setText(Integer.toString(attempt_counter));
+                        if (attempt_counter == 0)
+                            login_button.setEnabled(false);
                     }
                 }
-        );
-    }
+            }
+            );
+        }
 }
