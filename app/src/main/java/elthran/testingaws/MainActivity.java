47a7ebffd16c69eb7e4c859480fab6ae4e8c9432
elthran <jacobbrunner@gmail.com>
2018-09-14 14:57:00 -0700

    AWS Login and account creation seems to work

---
+++ app/src/main/java/elthran/testingaws/MainActivity.java
@@ -0,0 +1,28 @@
+package elthran.testingaws;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.amazonaws.mobile.auth.core.IdentityManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create log out Button on click listener
+        Button clickButton = (Button) findViewById(R.id.button_logout);
+        clickButton.setOnClickListener( new View.OnClickListener() {
+
+            public void onClick(View v) {
+                IdentityManager.getDefaultIdentityManager().signOut();
+                Intent intent = new Intent(MainActivity.this, AuthenticatorActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
